10460c89f08de81dd74d3ee91fe76dac9bfefb09
Usuario <Usuario@DESKTOP-698Q3OK>
2025-02-08 19:06:45 +0100

    JavaDoc

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,8 +1,11 @@
 package cuentas;
-
+/**
+ * gonzalez_blazquez_marta_ED04_Tarea
+ * ACTIVIDAD 1/JAVADOC. Añadir comentarios
+ */
 public class CCuenta {
 
-
+    //Atributos de la clase
     private String nombre;
     private String cuenta;
     private double saldo;
@@ -13,40 +16,40 @@ public class CCuenta {
     {
     }
 
-    public String getNombre() {
+    public String getNombre() {  //Obtiene nombre
         return nombre;
     }
 
-    public void setNombre(String nombre) {
+    public void setNombre(String nombre) { //Establece nombre
         this.nombre = nombre;
     }
 
-    public String getCuenta() {
+    public String getCuenta() { //Obtiene nº cuenta
         return cuenta;
     }
 
-    public void setCuenta(String cuenta) {
+    public void setCuenta(String cuenta) { //Establece nº de cuenta
         this.cuenta = cuenta;
     }
 
-    public double getSaldo() {
+    public double getSaldo() { //Obtiene saldo
         return saldo;
     }
 
-    public void setSaldo(double saldo) {
+    public void setSaldo(double saldo) { //Establece saldo
         this.saldo = saldo;
     }
 
-    public double getTipoInterés() {
+    public double getTipoInterés() { //Obtiene tipo de interés
         return tipoInterés;
     }
 
-    public void setTipoInterés(double tipoInterés) {
+    public void setTipoInterés(double tipoInterés) { //Establece tipo de interés
         this.tipoInterés = tipoInterés;
     }
-
+//Constructor que inicializa los atributos. falta tipo?
     public CCuenta(String nom, String cue, double sal, double tipo)
-    {
+    { 
         nombre =nom;
         cuenta=cue;
         saldo=sal;
@@ -54,22 +57,22 @@ public class CCuenta {
 
     public double estado()
     {
-        return saldo;
+        return saldo; //Devuelve saldo 
     }
-
+//Metodo para ingresar 
     public void ingresar(double cantidad) throws Exception
     {
-        if (cantidad<0)
-            throw new Exception("No se puede ingresar una cantidad negativa");
-        saldo = saldo + cantidad;
+        if (cantidad<0) //Funciona con condicion
+            throw new Exception("No se puede ingresar una cantidad negativa"); //si no se cumple condicion
+        saldo = saldo + cantidad;//aumenta saldo 
     }
-
+//Metodo para retirar
     public void retirar(double cantidad) throws Exception
     {
-        if (cantidad <= 0)
+        if (cantidad <= 0) 
             throw new Exception ("No se puede retirar una cantidad negativa");
         if (estado()< cantidad)
             throw new Exception ("No se hay suficiente saldo");
-        saldo = saldo - cantidad;
+        saldo = saldo - cantidad;//Disminuye el saldo
     }
 }
--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -1,28 +1,31 @@
 package cuentas;
-
+/**
+ * gonzalez_blazquez_marta_ED04_Tarea
+ * clase principal
+ */
 public class Main {
-
+ 
     public static void main(String[] args) {
-        operativa_cuenta(2500);
+        operativa_cuenta(2500); //llama al metodo operativa_cuenta con saldo inicial 2500
     }
 
     public static void operativa_cuenta(float cantidad) {
-        CCuenta cuenta1;
-        double saldoActual;
-
+        CCuenta cuenta1;//objeto de tipo CCuenta
+        double saldoActual;// variable para almacenar saldo actual
+        
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",cantidad,0);
-        saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        saldoActual = cuenta1.estado();//obtiene saldo actual
+        System.out.println("El saldo actual es"+ saldoActual );//lo imprime en pantalla
 
-        try {
+        try {//intenta retirar
             cuenta1.retirar(2300);
-        } catch (Exception e) {
+        } catch (Exception e) {//manejo de excepciones si falla al retirar
             System.out.print("Fallo al retirar");
         }
-        try {
+        try {//intenta ingresar
             System.out.println("Ingreso en cuenta");
             cuenta1.ingresar(695);
-        } catch (Exception e) {
+        } catch (Exception e) {//manejo de excepciones si falla al ingresar
             System.out.print("Fallo al ingresar");
         }
     }
